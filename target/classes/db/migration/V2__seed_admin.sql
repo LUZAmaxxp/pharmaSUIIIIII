fd192425e421d8b6e2b3fb4c4c1f009364fed094
LUZAmaxxp <allouchayman21@gmail.com>
2026-05-20 15:50:44 +0100

    chore: add .gitignore and untrack target/ directory

--- target/classes/db/migration/V2__seed_admin.sql
+++
@@ -1,6 +0,0 @@
--- V2: Seed initial admin user (password = Admin@1234 BCrypt hashed)
--- BCrypt hash of 'Admin@1234' with strength 12
-INSERT INTO users (email, password, role)
-VALUES ('admin@pharmacy.ma',
-        '$2a$12$RtCCUFQwGOLlIFj.lRF5SuyGUuqInzFqYBHSPbUxfT.bvM4WPovRa',
-        'ROLE_ADMIN');
